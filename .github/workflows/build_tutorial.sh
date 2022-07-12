3f1fdc6917f1893ae5ac80886fe1a9ffcf71f24c
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-07-12 15:28:28 +0200

    Working on the build_tutorial script

---
+++ .github/workflows/build_tutorial.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f "$0" ];
+then
+  echo "ERROR: $0 is not a file"
+fi
+
+folder=$(dirname "$0")
+file=$(basename "$0")
+
+echo ">> Weaving $0"
+julia --project -e "using JSOTutorials; JSOTutorials.weave_file($folder, $file)"
+
+# git clone https://github.com/JSOTemp/JSOTemp.github.io
