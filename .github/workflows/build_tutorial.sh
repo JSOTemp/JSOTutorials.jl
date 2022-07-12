c51259a4ba8ae25eee2f723ea6d1ea752add9ee4
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-07-12 16:17:53 +0200

    Working on the build_tutorial script

--- .github/workflows/build_tutorial.sh
+++ .github/workflows/build_tutorial.sh
@@ -10,6 +10,13 @@ folder=$(dirname "$ARG")
 file=$(basename "$ARG")
 
 echo ">> Weaving $file in $folder"
-julia --project -e "using Pkg; Pkg.instantiate(); using JSOTutorials; JSOTutorials.weave_file(\"$folder\", \"$file\")"
+julia --project -e """
+using Pkg;
+Pkg.instantiate();
+using JSOTutorials;
+folder = split(\"$folder\", \"/\")[2:end] # to remove tutorials prefix;
+folder = join(folder, \"/\");
+JSOTutorials.weave_file(folder, \"$file\")
+"""
 
 # git clone https://github.com/JSOTemp/JSOTemp.github.io
